eb0615be841064e3e3ef5a630f17b57017aa117e
Cheney Lin <lincheney@gmail.com>
2015-01-09 11:51:17 +1100

    dry up the specs for views and temp views

--- spec/object_table/temp_view_spec.rb
+++ spec/object_table/temp_view_spec.rb
@@ -2,9 +2,11 @@ require 'object_table'
 require 'object_table/temp_view'
 
 require 'support/object_table_example'
+require 'support/view_example'
 
 describe ObjectTable::TempView do
   it_behaves_like 'an object table', ObjectTable::TempView
+  it_behaves_like 'a table view', ObjectTable::TempView
 
   context 'with changes to the parent' do
     let(:table){ ObjectTable.new(col1: [1, 2, 3], col2: 5) }
@@ -73,148 +75,6 @@ describe ObjectTable::TempView do
       end
 
       subject.group(&block)
-    end
-  end
-
-  describe '#columns' do
-    let(:table){ ObjectTable.new(col1: [1, 2, 3], col2: 5) }
-
-    subject{ ObjectTable::TempView.new(table){ col1 > 2 } }
-
-    it 'should mask the columns of the parent table' do
-      mask = table.col1 > 2
-      table.columns.each do |k, v|
-        expect(subject.columns[k].to_a).to eql v[mask].to_a
-      end
-    end
-  end
-
-  describe '#set_column' do
-    let(:table) { ObjectTable.new(col1: [0, 1, 2, 3], col2: 5) }
-    let(:view)  { ObjectTable::TempView.new(table){ col1 > 0 } }
-
-    let(:column){ :col2 }
-    let(:value) { [10, 20, 30] }
-    let(:args)  { [] }
-
-    subject{ view.set_column(column, value, *args) }
-
-    context 'on an existing column' do
-      it 'should assign values to the column' do
-        subject
-        expect(view.columns[column].to_a).to eql value
-      end
-
-      it 'should not modify anything outside the view' do
-        subject
-        expect(table.columns[column].to_a).to eql [5] + value
-      end
-
-    end
-
-    context 'with a scalar' do
-      let(:value){ 10 }
-      it 'should fill the column with that value' do
-        subject
-        expect(view.columns[column].to_a).to eql ([value] * view.nrows)
-      end
-    end
-
-    context 'with a range' do
-      let(:value){ 0...3 }
-      it 'should assign the range values' do
-        subject
-        expect(view.columns[column].to_a).to eql value.to_a
-      end
-    end
-
-    context 'with the wrong length' do
-      let(:value) { [1, 2] }
-
-      it 'should fail' do
-        expect{subject}.to raise_error
-      end
-    end
-
-    context 'for a new column' do
-      let(:column){ :col3 }
-
-      it 'should create a new column' do
-        subject
-        expect(view.columns).to include column
-        expect(view.columns[column].to_a).to eql value
-      end
-
-      it 'should affect the parent table' do
-        subject
-        expect(table.columns).to include column
-      end
-
-      it 'should fill values outside the view with a default value' do
-        subject
-        default = NArray.new(table.columns[column].typecode, 1)[0]
-        expect(table.columns[column].to_a).to eql [default] + value
-      end
-
-      context 'with an NArray' do
-        let(:value){ NArray.int(3, 4, view.nrows).random! }
-
-        it 'should use the narray parameters' do
-          subject
-          expect(view.columns[column].to_a).to eql value.to_a
-        end
-      end
-
-       context 'when failed to add column' do
-        let(:value){ NArray[1, 2, 3] }
-
-        it 'should not have that column' do
-          expect(view).to receive(:add_column).with(column, value.typecode) do
-            view.columns[column] = ObjectTable::Column.make([0] * 10)
-            table.columns[column] = ObjectTable::Column.make([0] * 10)
-          end
-
-#           the assignment is going to chuck an error
-          subject rescue nil
-          expect(view.columns).to_not include column
-          expect(table.columns).to_not include column
-        end
-      end
-
-    end
-
-    context 'with an empty view' do
-      let(:view)  { ObjectTable::TempView.new(table){col1 < 0} }
-
-      context 'adding an empty column' do
-        let(:value) { [] }
-        it 'should add the column' do
-          subject
-          expect(view.columns[column].to_a).to eq value
-        end
-      end
-    end
-
-  end
-
-  describe '#pop_column' do
-    let(:table){ ObjectTable.new(col1: [1, 2, 3], col2: 5) }
-
-    let(:view) { ObjectTable::TempView.new(table){ col1 > 2 } }
-    let(:name)    { :col2 }
-
-    subject{ view.pop_column(name) }
-
-    it 'should remove the column' do
-      subject
-      expect(view.colnames).to_not include name
-      expect(view.columns).to_not include name
-    end
-
-    it 'should remove the column from the parent too' do
-      subject
-      expect(table.colnames).to_not include name
-      expect(table.columns).to_not include name
     end
   end
 
