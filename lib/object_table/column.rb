340bba1b339030a03ed491097185afb8fd5e88f9
Cheney Lin <lincheney@gmail.com>
2015-01-13 14:24:28 +1100

    add enumerator methods to column

--- lib/object_table/column.rb
+++ lib/object_table/column.rb
@@ -1,6 +1,12 @@
 require 'narray'
 
 class ObjectTable::Column < NArray
+  NARRAY_ENUMERATORS = [:to_a, :collect, :map, :sort, :all?, :any?, :max, :min, :none?]
+
+  NARRAY_ENUMERATORS.each{|method| alias_method "narray_#{method}", method}
+  include Enumerable
+  NARRAY_ENUMERATORS.each{|method| alias_method method, "narray_#{method}"}
+
   def self.make(value)
     value = case value
     when self
