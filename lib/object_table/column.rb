cffaa43e17bae65266bbd112e9efc8adb11653e5
Cheney Lin <lincheney@gmail.com>
2015-02-08 14:26:02 +1100

    slightly faster column stacking

--- lib/object_table/column.rb
+++ lib/object_table/column.rb
@@ -10,10 +10,10 @@ module ObjectTable::Column
     first_col = columns.first
     new_col = NArray.new(first_col.typecode, *first_col.shape[0...-1], new_rows)
 
-    row = 0
-    columns.each do |col|
-      new_col[false, row ... (row + col.shape[-1])] = col
-      row += col.shape[-1]
+    columns.reduce(0) do |row, col|
+      end_row = row + col.shape[-1]
+      new_col[false, row ... end_row] = col
+      end_row
     end
 
     new_col
