7a925c0d979ad7ce305d0980c5060913335f11ed
Cheney Lin <lincheney@gmail.com>
2015-02-08 20:39:30 +1100

    clean up the printing code

--- lib/object_table/printable.rb
+++ lib/object_table/printable.rb
@@ -1,22 +1,38 @@
 module ObjectTable::Printable
 
-  def self.get_printable_column(name, column)
-    rows = column.shape[-1].times.map do |i|
+  def self.get_printable_column(column)
+    column.shape[-1].times.map do |i|
       row = column[false, i]
       str = row.is_a?(NArray) ? row.inspect.partition("\n")[-1].strip : row.inspect
       str.split("\n")
     end
-
-    name = name.to_s
-    [[name]] + rows + [[name]]
   end
 
-  def self.get_printable_line_numbers(numbers)
-    rows = numbers.map do |i|
-      ["#{i}: "]
+  def self.calc_column_widths(columns)
+    columns.map{|col| col.flatten.map(&:length).max}
+  end
+
+  def _format_section(row_slice)
+    numbers = row_slice.map{|i| ["#{i}: "]}
+    section = columns.map do |name, c|
+      c = c.slice(false, row_slice)
+      ObjectTable::Printable.get_printable_column(c)
     end
 
-    [['']] + rows + [['']]
+    [numbers] + section
+  end
+
+  def _format_rows(rows, widths)
+    rows.flat_map do |row|
+      height = row.map(&:length).max
+
+      row = row.zip(widths).map do |cell, width|
+        cell += [" "] * (height - cell.length)
+        cell.map{|i| i.rjust(width)}
+      end
+
+      row.transpose.map(&:join)
+    end
   end
 
   def inspect(max_section = 5, col_padding = 2)
@@ -25,43 +41,29 @@ module ObjectTable::Printable
     return (header + "(empty table)") if ncols == 0
     return (header + "(empty table with columns: #{colnames.join(", ")})") if nrows == 0
 
-    printed_columns = []
+    column_headers = [''] + colnames.map(&:to_s)
 
     if nrows > max_section * 2
-      head = (0...max_section)
-      tail = ((nrows - max_section)...nrows)
+      head = _format_section(0 ... max_section)
+      tail = _format_section((nrows - max_section) ... nrows)
 
-      printed_columns.push ObjectTable::Printable.get_printable_line_numbers(head.to_a + tail.to_a)
+      columns = [column_headers, head, tail].transpose
+      widths = NArray.to_na(ObjectTable::Printable.calc_column_widths(columns)) + col_padding
+      total_width = widths.sum
 
-      printed_columns += columns.map do |name, c|
-        c = c.slice(false, [head, tail])
-        ObjectTable::Printable.get_printable_column(name, c)
-      end
+      rows = _format_rows(head.transpose, widths)
+      rows.push('-' * total_width)
+      rows += _format_rows(tail.transpose, widths)
+
     else
-      max_section = -1
-      printed_columns.push ObjectTable::Printable.get_printable_line_numbers(0...nrows)
-      printed_columns += columns.map do |name, c|
-        ObjectTable::Printable.get_printable_column(name, c)
-      end
+      section = _format_section(0...nrows)
+      columns = [column_headers, section].transpose
+      widths = NArray.to_na(ObjectTable::Printable.calc_column_widths(columns)) + col_padding
+      rows = _format_rows(section.transpose, widths)
     end
 
-    widths = printed_columns.map{|row| row.flat_map{|c| c.map(&:length)}.max + col_padding}
-
-    header + printed_columns.transpose.each_with_index.map do |row, index|
-      height = row.map(&:length).max
-
-      row = row.zip(widths).map do |cell, width|
-        cell += [" "] * (height - cell.length)
-        cell.map{|i| i.rjust(width)}
-      end
-
-      row = row.transpose.map{|i| i.join('')}.join("\n")
-
-      if index == max_section
-        row += "\n" + '-'*widths.reduce(:+)
-      end
-      row
-    end.join("\n")
+    column_headers = _format_rows([[column_headers].transpose], widths).join
+    header + ([column_headers] + rows + [column_headers]).join("\n")
 
   rescue NoMethodError => e
     raise Exception.new(e)
