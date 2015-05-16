fe34b94a64acfb6b0cb14a0f4463a20bcc873621
Cheney Lin <lincheney@gmail.com>
2015-05-17 01:22:47 +1000

    refactor printable

--- lib/object_table/printable.rb
+++ lib/object_table/printable.rb
@@ -1,69 +1,66 @@
 module ObjectTable::Printable
 
-  def self.get_printable_column(column)
-    column.shape[-1].times.map do |i|
-      row = column[false, i]
-      str = row.is_a?(NArray) ? row.inspect.partition("\n")[-1].strip : row.inspect
-      str.split("\n")
-    end
-  end
-
-  def self.calc_column_widths(columns)
-    columns.map{|col| col.flatten.map(&:length).max}
-  end
-
-  def _format_section(row_slice)
-    numbers = row_slice.map{|i| ["#{i}: "]}
-    section = columns.map do |name, c|
-      c = c.slice(false, row_slice)
-      ObjectTable::Printable.get_printable_column(c)
+  module Helper
+    def self.format_column(column)
+      return column.to_a.map(&:inspect) if column.rank < 2
+      column.shape[-1].times.map do |i|
+        row = column[false, i]
+        row.inspect.partition("\n")[-1].strip
+      end
     end
 
-    [numbers] + section
-  end
+    def self.split_column_lines(name, column)
+      [name, *column, name].map{|i| i.split("\n")}
+    end
 
-  def _format_rows(rows, widths)
-    rows.flat_map do |row|
-      height = row.map(&:length).max
+    def self.calc_column_widths(rows, padding)
+      columns = rows.transpose
+      columns.map{|col| col.flatten.map(&:length).max + padding}
+    end
 
-      row = row.zip(widths).map do |cell, width|
-        cell += [" "] * (height - cell.length)
-        cell.map{|i| i.rjust(width)}
+    def self.format_rows(rows, widths)
+      rows = rows.flat_map do |row|
+        height = row.map(&:length).max
+        row.map{|cell| cell.fill('', cell.length...height)}.transpose
       end
 
-      row.transpose.map(&:join)
+      format = widths.to_a.map{|w| "%#{w}s"}.join
+      rows.map{|row| format % row }
     end
   end
 
-  def inspect(max_section = 5, col_padding = 2)
-    header = "#{self.class}(#{nrows}, #{ncols})\n"
 
-    return (header + "(empty table)") if ncols == 0
-    return (header + "(empty table with columns: #{colnames.join(", ")})") if nrows == 0
+  def _format_section(row_slice, slice)
+    numbers = Helper.split_column_lines('', row_slice.map{|i| "#{i}: "})
 
-    column_headers = [''] + colnames.map(&:to_s)
-
-    if nrows > max_section * 2
-      head = _format_section(0 ... max_section)
-      tail = _format_section((nrows - max_section) ... nrows)
-
-      columns = [column_headers, head, tail].transpose
-      widths = NArray.to_na(ObjectTable::Printable.calc_column_widths(columns)) + col_padding
-      total_width = widths.sum
-
-      rows = _format_rows(head.transpose, widths)
-      rows.push('-' * total_width)
-      rows += _format_rows(tail.transpose, widths)
-
-    else
-      section = _format_section(0...nrows)
-      columns = [column_headers, section].transpose
-      widths = NArray.to_na(ObjectTable::Printable.calc_column_widths(columns)) + col_padding
-      rows = _format_rows(section.transpose, widths)
+    section = columns.map do |name, c|
+      c = c.slice(false, row_slice)
+      c = Helper.format_column(c)
+      c = Helper.split_column_lines(name.to_s, c)
     end
 
-    column_headers = _format_rows([[column_headers].transpose], widths).join
-    header + ([column_headers] + rows + [column_headers]).join("\n")
+    ([numbers] + section).map{|col| col.slice(slice)}
+  end
+
+
+  def inspect(max_section=5, col_padding=2)
+    header = "#{self.class}(#{nrows}, #{ncols})\n"
+
+    return "#{header}(empty table)" if ncols == 0
+    return "#{header}(empty table with columns: #{colnames.join(", ")})" if nrows == 0
+
+    separated = (nrows > max_section * 2)
+    max_section = (nrows / 2) unless separated
+
+    head = _format_section(0...max_section, 0...-1).transpose
+    tail = _format_section((nrows - max_section)...nrows, 1..-1).transpose
+    widths = Helper.calc_column_widths(head + tail, col_padding)
+
+    rows = Helper.format_rows(head, widths)
+    rows.push('-' * widths.reduce(:+)) if separated
+    rows.concat Helper.format_rows(tail, widths)
+
+    header + rows.join("\n")
 
   rescue NoMethodError => e
     raise Exception.new(e)
