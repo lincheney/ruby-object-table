699e57d46efc5dbcf77406aa6ae1b09f6875b840
Cheney Lin <lincheney@gmail.com>
2015-05-17 17:44:20 +1000

    better tests for left join

--- spec/object_table_spec.rb
+++ spec/object_table_spec.rb
@@ -270,23 +270,42 @@ describe ObjectTable do
 
     let(:left) do
       ObjectTable.new(
-        key1:   key1[0...lgroups] * lsize,
-        key2:   key2[0...lgroups] * lsize,
-        lval1:  NArray.float(lgroups * lsize).random,
-        lval2:  NArray.float(10, lgroups * lsize).random,
+        key1:   key1[lkeys] * lsize,
+        key2:   key2[lkeys] * lsize,
+        lval1:  NArray.object(lgroups * lsize).map!{rand},
+        lval2:  NArray.object(10, lgroups * lsize).map!{rand},
         )
     end
 
     let(:right) do
       ObjectTable.new(
-        key1:   key1[-rgroups..-1] * rsize,
-        key2:   key2[-rgroups..-1] * rsize,
-        rval1:  NArray.float(rgroups * rsize).random,
+        key1:   key1[rkeys] * rsize,
+        key2:   key2[rkeys] * rsize,
+        rval1:  NArray.object(rgroups * rsize).map!{rand},
         )
     end
 
+    subject           { left.join(right, :key1, :key2, type: join_type) }
+    let(:common)      { subject.where{lval1.ne(nil).and(rval1.ne(nil))}.clone }
+    let(:left_only)   { subject.where{rval1.eq nil}.clone }
+    let(:right_only)  { subject.where{lval1.eq nil}.clone }
+
+    let(:expected_left_only) do
+      a = left.apply{[key1.to_a, key2.to_a]}.transpose
+      b = [key1[0...-rgroups], key2[0...-rgroups]].transpose
+      mask = a.map{|k| b.include?(k) ? 1 : 0}
+      left.where{NArray.to_na(mask).to_type('byte')}
+    end
+
+    let(:expected_right_only) do
+      a = right.apply{[key1.to_a, key2.to_a]}.transpose
+      b = [key1[lgroups..-1], key2[lgroups..-1]].transpose
+      mask = a.map{|k| b.include?(k) ? 1 : 0}
+      right.where{NArray.to_na(mask).to_type('byte')}
+    end
+
     context 'inner join' do
-      subject{ left.join(right, :key1, type: 'inner') }
+      let(:join_type) { 'inner' }
 
       it 'shold have all columns' do
         expect(subject.colnames).to eql [:key1, :key2, :lval1, :lval2, :rval1]
@@ -321,19 +340,53 @@ describe ObjectTable do
     end
 
     context 'left join' do
-      subject{ left.join(right, key=:key1, type: 'left') }
+      let(:join_type) { 'left' }
 
-      let(:result) do
-        ObjectTable.new(
-          key1:   [ "b",  "b",  "c",  "a",  "a"],
-          lvalue: [   1,    3,    4,    0,    2],
-          rvalue: [   2,    2,    1,    0,    0],
-          )
+      it 'shold have all columns' do
+        expect(subject.colnames).to eql [:key1, :key2, :lval1, :lval2, :rval1]
       end
 
-      it 'should return the joined result' do
-        expect(subject).to eql result
+      it 'should only have common and left keys' do
+        expect(subject.key1.to_a).to_not include(*key1[lgroups...-1])
+        expect(subject.key2.to_a).to_not include(*key2[lgroups...-1])
       end
+
+      it 'should duplicate keys correctly' do
+        counts = common.group_by(:key1, :key2).apply{ nrows }
+        expect(counts.v_0.to_a).to eq ([lsize * rsize] * common_keys.size)
+      end
+
+      it 'should cross product the values' do
+        common.group_by(:key1, :key2).each do |grp|
+          filter = Proc.new{|t| t.key1.eq(grp.K.key1).and(t.key2.eq(grp.K.key2)) }
+          lgroup = left.where(&filter)
+          rgroup = right.where(&filter)
+
+          lvalues = lgroup.apply{[lval1.to_a, lval2.to_a]}.transpose
+          rvalues = rgroup.apply{[rval1.to_a]}.transpose
+          joined_values = grp.apply{[lval1, lval2, rval1]}.map(&:to_a).transpose
+
+          expected = lvalues.product(rvalues).map{|row| row.flatten(1)}
+          expect(joined_values).to eq expected
+        end
+      end
+
+      describe 'with missing right keys' do
+        it 'should have the left keys' do
+          counts = left_only.group_by(:key1, :key2).apply{ nrows }
+          expect(counts.v_0.to_a).to eq ([lsize] * (groups - rgroups))
+        end
+
+        it 'should fill the right values with nil' do
+          expect(left_only.rval1.to_a).to eq [nil] * left_only.nrows
+        end
+
+        it 'should keep the left values' do
+          left_only.pop_column(:rval1)
+          expect(left_only).to eq expected_left_only
+        end
+      end
+
     end
 
     context 'right join' do
